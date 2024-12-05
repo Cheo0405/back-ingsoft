fa0259c13b7f2f67a5226f6dd5607ba145a9bede
CarlosLinero <105866630+Cheo0405@users.noreply.github.com>
2024-12-05 10:00:07 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@
 //Creo el servidor por donde se va a desplegar la aplicacion con ayuda de express
 //https:localhost/4000
 import app from './app.js'
+import dotenv from "dotenv";
 import {connectDB} from "./db.js"
 
+dotenv.config();
 connectDB();
-app.listen(4000)
-console.log("Server on port", 4000)
+app.listen(
+    process.env.PORT,
+    console.log("Servidor en el puerto " + process.env.PORT)
+  );
